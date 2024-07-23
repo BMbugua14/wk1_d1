d93d971a3d529f9bbf12a18e59f709eb7d584296
Brian-Mbugua <bmbugua60@gmail.com>
2024-07-23 16:40:17 -0400

    initial commit

---
+++ homework.py
@@ -0,0 +1,42 @@
+#Task1
+#weather = "sunny"
+
+#if weather == "sunny":
+#print("Wear sunglasses!")
+#else:
+#print("Take an umbrella!")
+
+#Answer1
+weather = "sunny"
+
+if weather == "sunny":
+    print("Wear sunglasses!")
+else:
+    print("Take an umbrella!")
+
+#Answer2
+mood = "Happy"
+input("how do you feel today?")
+if mood =="Happy":
+    print("I hope your day gets better!")
+if mood == "Sad":
+    print("I hope your day gets better!")    
+
+#Task2
+variable_a = "Hello, World!"
+variable_b = 23
+variable_c = 3.14
+variable_d = True
+
+print(type(variable_a))
+print(type(variable_b))
+print(type(variable_c))
+print(type(variable_d))
+
+#Task3
+bread = 2.75
+peanut_butter= 4.50
+jelly = 3.23
+
+#Answer1
+print(bread + jelly + peanut_butter)
